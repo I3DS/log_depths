d5734b8e5b2b73939781cef63e94219ba77a852d
Fredrik Bakkevig Haugli <fredrik.haugli@sintef.no>
2022-07-09 12:39:52 +0200

    Add option to write whole dm to file

--- main.cpp
+++ main.cpp
@@ -18,19 +18,37 @@ void signal_handler(int)
     running = false;
 }
 
-void handle_frame(i3ds::DepthMap &dm, const unsigned int n_measurements)
+void update_captured(const unsigned int &n_measurements) {
+  captured++;
+  if (n_measurements != 0 && captured >= n_measurements) {
+    running = false;
+  }
+}
+
+void write_center_value(i3ds::DepthMap &dm, const unsigned int n_measurements) {
+  float center_value =
+      dm.depths[(dm.descriptor.width / 2) * (dm.descriptor.height + 1)];
+  std::cout << center_value << "\n";
+  {
+    std::lock_guard<std::mutex> guard(mtx);
+    logfile << center_value << "\n";
+    update_captured(n_measurements);
+  }
+}
+
+void write_whole_dm(i3ds::DepthMap &dm, const unsigned int n_measurements)
 {
-    float center_value = dm.depths[(dm.descriptor.width/2)*(dm.descriptor.height+1)];
-    std::cout << center_value << "\n";
+    size_t size = dm.descriptor.depth_size*dm.descriptor.height*dm.descriptor.width;
     {
-        logfile << center_value << "\n";
         std::lock_guard<std::mutex> guard(mtx);
-        captured++;
-        if (n_measurements != 0 && captured >= n_measurements) {
-            running = false;
+        for (int i = 0; i < size; ++i)
+        {
+            logfile.write(reinterpret_cast<char*>(&descriptorsValues[i]), sizeof(float));
         }
+        update_captured(n_measurements);
     }
 }
+
 
 int main(int argc, char *argv[])
 {
@@ -38,6 +56,7 @@ int main(int argc, char *argv[])
     captured = 0;
     int node;
     unsigned int n_measurements;
+    bool whole_dm;
     std::string outfilename;
     i3ds::Configurator configurator;
     po::options_description desc("Captures ToF depthmaps and logs the center value to file\n  Available options");
@@ -46,15 +65,23 @@ int main(int argc, char *argv[])
         ("node,n", po::value<int>(&node)->default_value(10), "Node ID of camera")
         ("measurements,m", po::value<unsigned int>(&n_measurements)->default_value(0), "Number of measurements to capture (0 means no limit)")
         ("output,o", po::value(&outfilename)->default_value("tof.log"), "Output file")
+        ("whole_dm,w", po::bool_switch(&whole_dm)->default_value(false), "Log whole depth map to file")
         ;
     po::variables_map vm = configurator.parse_common_options(desc, argc, argv);
-
-    logfile.open(outfilename, std::ofstream::out);
 
     std::cout << "Connecting to Node with ID: " << node << std::endl;
     i3ds::Context::Ptr context = i3ds::Context::Create();
     i3ds::Subscriber subscriber(context);
-    subscriber.Attach<i3ds::ToFCamera::MeasurementTopic>(node, [n_measurements](auto d){handle_frame(d, n_measurements);});
+
+    if (whole_dm) {
+        logfile.open(outfilename, std::ofstream::out | std::ofstream::binary);
+        subscriber.Attach<i3ds::ToFCamera::MeasurementTopic>(node, [n_measurements](auto d){write_whole_dm(d, n_measurements);});
+    }
+    else {
+        logfile.open(outfilename, std::ofstream::out);
+        subscriber.Attach<i3ds::ToFCamera::MeasurementTopic>(node, [n_measurements](auto d){write_center_value(d, n_measurements);});
+    }
+
     subscriber.Start();
 
     signal(SIGINT, signal_handler);
